7a76c2402348ac145f6333f04678332edbf76e55
Riacrdo Consigliere <ricardo.consigliere.f@mail.pucv.cl>
2021-03-23 14:21:26 -0300

    Respuestas servidor-cliente

--- server.js
+++ server.js
@@ -1,21 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const response = require('./network/response');
 
 var app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.use(router);
 
 router.get('/message', function (req, res){
-    res.send('Lista de mensajes');
+    console.log(req.headers);
+    res.header({
+        "custom-header": "Nuestro valor personalizado",
+    });
+    console.log(req.query);
+    console.log(req.body);
+    if (req.query.error == "ok"){
+        response.error(req, res, 'Error simulado');
+    }else{
+        response.success(req, res, 'Lista de mensajes');
+    }
 });
 router.post('/message', function (req, res){
-    res.send('Se añadió el mensaje');
+    console.log(req.query);
+    console.log(req.body);
+    if (req.query.error == "ok"){
+        response.error(req, res, 'Error simulado', 401);
+    }else{
+        response.success(req, res, 'Creado correctamente', 201);
+    }
 });
 router.delete('/message', function (req, res){
     res.send('Se eliminó el mensaje');
 });
 router.patch('/message', function (req, res){
-    res.send('Se actualizó el mensaje');
+    res.status(201).send({error:'', body: 'Actualizado Correctamente'});
 });
 router.put('/message', function (req, res){
     res.send('Se modificó el mensaje');
