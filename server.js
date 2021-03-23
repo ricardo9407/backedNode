e1ae855b0c3fa62919e42c0a3ef9937a0b5f14e9
Riacrdo Consigliere <ricardo.consigliere.f@mail.pucv.cl>
2021-03-23 15:10:33 -0300

    Errores con seguridad

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ router.post('/message', function (req, res){
     console.log(req.query);
     console.log(req.body);
     if (req.query.error == "ok"){
-        response.error(req, res, 'Error simulado', 401);
+        response.error(req, res, 'Error inesperado', 500, 'Es solo una simulacion de los errores');
     }else{
         response.success(req, res, 'Creado correctamente', 201);
     }
