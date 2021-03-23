2ce6a72e674ea54538c9e74c6cd249bbe3e54bb0
Riacrdo Consigliere <ricardo.consigliere.f@mail.pucv.cl>
2021-03-23 11:20:09 -0300

    Como hacer distintas peticiones

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const router = express.Router();
+
 var app = express();
-app.use('/', function (req, res){
-    res.send('Hola');
+
+app.use(router);
+
+router.get('/message', function (req, res){
+    res.send('Lista de mensajes');
 });
+router.post('/message', function (req, res){
+    res.send('Se añadió el mensaje');
+});
+router.delete('/message', function (req, res){
+    res.send('Se eliminó el mensaje');
+});
+router.patch('/message', function (req, res){
+    res.send('Se actualizó el mensaje');
+});
+router.put('/message', function (req, res){
+    res.send('Se modificó el mensaje');
+});
+
 app.listen(3000);
 console.log('La aplicacion esta escuchando en http://localhost:3000');
